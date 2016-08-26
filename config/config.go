alphagov-mirror/paas-gorouter

Allow forcing X-Forwarded-Proto Header
There are situations where it's not possible to determine the
appropriate value for this header from the incoming connection (for
example when running behind an Amazon ELB in TCP mode where SSL
termination is handled by the ELB). This therefore adds a config option
to allow specifying a value that should be unconditionally added to all
downstream requests.

config/config.go
@@ -76,23 +76,24 @@ var defaultLoggingConfig = LoggingConfig{
 }
 
 type Config struct {
-	Status                   StatusConfig  `yaml:"status"`
-	Nats                     []NatsConfig  `yaml:"nats"`
-	Logging                  LoggingConfig `yaml:"logging"`
-	Port                     uint16        `yaml:"port"`
-	Index                    uint          `yaml:"index"`
-	Zone                     string        `yaml:"zone"`
-	GoMaxProcs               int           `yaml:"go_max_procs,omitempty"`
-	TraceKey                 string        `yaml:"trace_key"`
-	AccessLog                AccessLog     `yaml:"access_log"`
-	EnableAccessLogStreaming bool          `yaml:"enable_access_log_streaming"`
-	DebugAddr                string        `yaml:"debug_addr"`
-	EnableSSL                bool          `yaml:"enable_ssl"`
-	SSLPort                  uint16        `yaml:"ssl_port"`
-	SSLCertPath              string        `yaml:"ssl_cert_path"`
-	SSLKeyPath               string        `yaml:"ssl_key_path"`
-	SSLCertificate           tls.Certificate
-	SSLSkipValidation        bool `yaml:"ssl_skip_validation"`
+	Status                    StatusConfig  `yaml:"status"`
+	Nats                      []NatsConfig  `yaml:"nats"`
+	Logging                   LoggingConfig `yaml:"logging"`
+	Port                      uint16        `yaml:"port"`
+	Index                     uint          `yaml:"index"`
+	Zone                      string        `yaml:"zone"`
+	GoMaxProcs                int           `yaml:"go_max_procs,omitempty"`
+	TraceKey                  string        `yaml:"trace_key"`
+	AccessLog                 AccessLog     `yaml:"access_log"`
+	EnableAccessLogStreaming  bool          `yaml:"enable_access_log_streaming"`
+	DebugAddr                 string        `yaml:"debug_addr"`
+	EnableSSL                 bool          `yaml:"enable_ssl"`
+	SSLPort                   uint16        `yaml:"ssl_port"`
+	SSLCertPath               string        `yaml:"ssl_cert_path"`
+	SSLKeyPath                string        `yaml:"ssl_key_path"`
+	SSLCertificate            tls.Certificate
+	SSLSkipValidation         bool   `yaml:"ssl_skip_validation"`
+	ForceForwardedProtoHeader string `yaml:"force_forwarded_proto_header"`
 
 	CipherString string `yaml:"cipher_suites"`
 	CipherSuites []uint16

hpcloud/gorouter

config/config.go
@@ -77,24 +77,25 @@ var defaultLoggingConfig = LoggingConfig{
 }
 
 type Config struct {
-	Status                   StatusConfig  `yaml:"status"`
-	Nats                     []NatsConfig  `yaml:"nats"`
-	Logging                  LoggingConfig `yaml:"logging"`
-	Port                     uint16        `yaml:"port"`
-	Index                    uint          `yaml:"index"`
-	Zone                     string        `yaml:"zone"`
-	GoMaxProcs               int           `yaml:"go_max_procs,omitempty"`
-	TraceKey                 string        `yaml:"trace_key"`
-	AccessLog                AccessLog     `yaml:"access_log"`
-	EnableAccessLogStreaming bool          `yaml:"enable_access_log_streaming"`
-	DebugAddr                string        `yaml:"debug_addr"`
-	EnablePROXY              bool          `yaml:"enable_proxy"`
-	EnableSSL                bool          `yaml:"enable_ssl"`
-	SSLPort                  uint16        `yaml:"ssl_port"`
-	SSLCertPath              string        `yaml:"ssl_cert_path"`
-	SSLKeyPath               string        `yaml:"ssl_key_path"`
-	SSLCertificate           tls.Certificate
-	SkipSSLValidation        bool `yaml:"skip_ssl_validation"`
+	Status                    StatusConfig  `yaml:"status"`
+	Nats                      []NatsConfig  `yaml:"nats"`
+	Logging                   LoggingConfig `yaml:"logging"`
+	Port                      uint16        `yaml:"port"`
+	Index                     uint          `yaml:"index"`
+	Zone                      string        `yaml:"zone"`
+	GoMaxProcs                int           `yaml:"go_max_procs,omitempty"`
+	TraceKey                  string        `yaml:"trace_key"`
+	AccessLog                 AccessLog     `yaml:"access_log"`
+	EnableAccessLogStreaming  bool          `yaml:"enable_access_log_streaming"`
+	DebugAddr                 string        `yaml:"debug_addr"`
+	EnablePROXY               bool          `yaml:"enable_proxy"`
+	EnableSSL                 bool          `yaml:"enable_ssl"`
+	SSLPort                   uint16        `yaml:"ssl_port"`
+	SSLCertPath               string        `yaml:"ssl_cert_path"`
+	SSLKeyPath                string        `yaml:"ssl_key_path"`
+	SSLCertificate            tls.Certificate
+	SkipSSLValidation         bool   `yaml:"skip_ssl_validation"`
+	ForceForwardedProtoHeader string `yaml:"force_forwarded_proto_header"`
 
 	CipherString string `yaml:"cipher_suites"`
 	CipherSuites []uint16